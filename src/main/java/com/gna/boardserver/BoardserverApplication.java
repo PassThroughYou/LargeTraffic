e755d84d2e76fbd2b76c808482a953b817f3331b
GinaYu <105768159+stargnastar@users.noreply.github.com>
2024-08-19 16:35:36 +0900

    [fix] 초기설정

--- src/main/java/com/gna/boardserver/BoardserverApplication.java
+++ src/main/java/com/gna/boardserver/BoardserverApplication.java
@@ -1,4 +1,4 @@
-package com.largetraffic.boardserver;
+package com.gna.boardserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardserverApplication.class, args);
+    }
 
 }
--- src/test/java/com/gna/boardserver/BoardserverApplicationTests.java
+++ src/test/java/com/gna/boardserver/BoardserverApplicationTests.java
@@ -1,4 +1,4 @@
-package com.largetraffic.boardserver;
+package com.gna.boardserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BoardserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
